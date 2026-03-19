b2446b590ae012f456f5a04e19bf7bf5b3bed4c2
lokeshbohra7500 <lokeshbohra7500@gmail.com>
2026-03-19 11:41:56 +0530

    add test setup

---
+++ user-service/user-service/src/test/java/com/bookinn/userservice/UserServiceTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserServiceTest {
+
+    @Test
+    void testBasic() {
+        assertEquals(4, 2 + 2);
+    }
+}
